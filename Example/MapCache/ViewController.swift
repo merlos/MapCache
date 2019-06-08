da0b8adef9f205ec4361ca8e7c2dae932d814db1
merlos <merlos@users.noreply.github.com>
2019-06-07 21:35:55 -0400

    Added button in example app to display cache size and a button to clear the cache

--- Example/MapCache/ViewController.swift
+++ Example/MapCache/ViewController.swift
@@ -14,15 +14,35 @@ import MapCache
 class ViewController: UIViewController {
     
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var updateSizeButton: UIButton!
+    @IBOutlet weak var clearCacheButton: UIButton!
+    @IBOutlet weak var cacheSizeLabel: UILabel!
+    
+    var config: MapCacheConfig = MapCacheConfig(withTileUrlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         map.delegate = self
-        let config = MapCacheConfig(withTileUrlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
         map.useMapCache(withConfig: config)
     }
-
+    
+    
+    @IBAction func updateSize(_ sender: Any) {
+        print("update cache size")
+        let discCache = DiskCache(withName: config.cacheName)
+        cacheSizeLabel.text = String(discCache.calculateSize())
+    }
+    
+    
+    @IBAction func clearCache(_ sender: Any) {
+        print("clear cache")
+        let discCache = DiskCache(withName: config.cacheName)
+        discCache.removeAllData({
+            self.cacheSizeLabel.text = String(discCache.calculateSize())
+        })
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- MapCache/Classes/MapCache.swift
+++ MapCache/Classes/MapCache.swift
@@ -34,7 +34,7 @@ public class MapCache : NSObject {
         
     }
     
-    func clear() {
+    func removeAll() {
         
     }
     
