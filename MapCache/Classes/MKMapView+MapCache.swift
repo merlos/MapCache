9f8f4e760bb20567ffe5f6062d6c7f2db24bb1b5
merlos <merlos@users.noreply.github.com>
2019-06-15 11:45:27 -0400

    added documentation to MKMapView+MapCache

--- MapCache/Classes/MKMapView+MapCache.swift
+++ MapCache/Classes/MKMapView+MapCache.swift
@@ -8,8 +8,12 @@
 import Foundation
 import MapKit
 
+/// Extension that provides MKMapView support to use MapCache
+///
+/// - SeeAlso: Readme documentation
 extension MKMapView {
 
+    /// Will tell the map to use the cache passed as parameter.
     public func useCache(_ cache: MapCache) {
         
         let tileServerOverlay = CachedTileOverlay(withCache: cache)
@@ -17,6 +21,10 @@ extension MKMapView {
         self.insertOverlay(tileServerOverlay, at: 0, level: .aboveLabels)
     }
     
+    /// Call this method within the MKMapView delegate function
+    /// `mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer`
+    ///
+    /// - SeeAlso: Example project and Readme documentation
     public func mapCacheRenderer(forOverlay overlay: MKOverlay) -> MKOverlayRenderer {
         if overlay.isKind(of: MKTileOverlay.self) {
             return MKTileOverlayRenderer(overlay: overlay)
