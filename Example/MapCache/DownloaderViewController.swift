ce70a2b2f7e7e2d0a293e34fe94fa3802a67c194
merlos <merlos@users.noreply.github.com>
2019-07-10 07:10:43 -0400

    remove spaces

--- Example/MapCache/DownloaderViewController.swift
+++ Example/MapCache/DownloaderViewController.swift
@@ -30,27 +30,23 @@ class DownloaderViewController: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-
         mapView.delegate = self
-        
         //Initialize the region with any random value.
         region = TileCoordsRegion(topLeftLatitude: 10.0, topLeftLongitude: 10.0, bottomRightLatitude: 20.0, bottomRightLongitude: 20.0, minZoom: 1, maxZoom: 9)
-        
     }
     
     
     /// Activated when download region button is pressed
     @IBAction func downloadRegion(_ sender: Any) {
         print("Download Region Pressed!")
-        
         let config = MapCacheConfig()
         let mapCache = MapCache(withConfig: config)
         let downloader = RegionDownloader(forRegion: region!, mapCache: mapCache)
         let delegate = self
         downloader.delegate = delegate
         downloader.start()
-        
     }
+    
     /// Slider changed its value
     @IBAction func sliderValueChanged(_ sender: Any) {
         print("Slider value Changed \(Zoom(slider.value))" )
