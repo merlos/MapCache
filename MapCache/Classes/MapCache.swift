68dbebc3772d8a91a675f9a166a9994ada7bab58
merlos <merlos@users.noreply.github.com>
2020-05-10 06:58:13 -0400

    refactor MapCache::loadTile to make use of LoadTileMode

--- MapCache/Classes/MapCache.swift
+++ MapCache/Classes/MapCache.swift
@@ -27,7 +27,7 @@ public class MapCache : MapCacheProtocol {
         urlString = urlString.replacingOccurrences(of: "{x}", with: String(path.x))
         urlString = urlString.replacingOccurrences(of: "{y}", with: String(path.y))
         urlString = urlString.replacingOccurrences(of: "{s}", with: config.roundRobinSubdomain() ?? "")
-        print("MapCache::url() urlString: \(urlString)")
+        Log.debug(message: "MapCache::url() urlString: \(urlString)")
         return URL(string: urlString)!
     }
     
@@ -35,51 +35,65 @@ public class MapCache : MapCacheProtocol {
         return "\(config.urlTemplate)-\(path.x)-\(path.y)-\(path.z)"
     }
     
+    
     public func loadTile(at path: MKTileOverlayPath, result: @escaping (Data?, Error?) -> Void) {
-        // Use cache
-        // is the file alread in the system?
+        
         let key = cacheKey(forPath: path)
         
-        let loadTileFromOrigin = { () -> () in
+        // Feches tile from server. If it is found updates the cache
+        func fetchTileFromServer(failure fail: ((Error?) -> ())? = nil,
+                                 success succeed: @escaping (Data) -> ()) {
             let url = self.url(forTilePath: path)
-            print ("MapCache::loadTile() url=\(url)")
+            print ("MapCache::fetchTileFromServer() url=\(url)")
             let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
                 if error != nil {
-                    print("!!! MapCache::loadTile Error for key= \(key)")
-                    result(nil, error)
+                    print("!!! MapCache::fetchTileFromServer Error for url= \(url)")
+                    fail!(error)
                     return
                 }
                 guard let data = data else {
-                    result(nil, nil)
+                    fail!(nil)
                     return
                 }
                 self.diskCache.setData(data, forKey: key)
-                print ("CachedTileOverlay:: Data received saved cacheKey=\(key)" )
-                result(data,nil)
+                print ("MapCache::fetchTileFromServer:: Data received saved cacheKey=\(key)" )
+                succeed(data)
             }
             task.resume()
         }
         
-        // If fetching data from cache is successfull => return the data
-        let fetchSuccess = {(data: Data) -> () in
-            print ("MapCache::loadTile() found! cacheKey=\(key)" )
-            result (data, nil)
-        }
-        // Closure to run if error found while fetching data from cache
-        let fetchFailure = { (error: Error?) -> () in
-            print ("MapCache::loadTile() Not found! cacheKey=\(key)" )
-            loadTileFromOrigin()
+       // Tries to load the tile from the server.
+       // If it fails returns error to the caller.
+        let tileFromServerFallback = { () -> () in
+            print ("MapCache::tileFromServerFallback:: key=\(key)" )
+            fetchTileFromServer(failure: {error in result(nil, error)},
+                                success: {data in result(data, nil)})
         }
         
-        switch mode {
+        // Tries to load the tile from the cache.
+        // If it fails returns error to the caller.
+        let tileFromCacheFallback = { () -> () in
+            self.diskCache.fetchDataSync(forKey: key,
+                    failure: {error in result(nil, error)},
+                    success: {data in result(data, nil)})
             
-            case .always_server:
-                loadTileFromOrigin()
-            case .cache_then_server:
-                diskCache.fetchDataSync(forKey: key, failure: fetchFailure, success: fetchSuccess)
-            case .cache_only:
-                diskCache.fetchDataSync(forKey: key, failure: { error in result(nil, error)}, success: fetchSuccess)
-    
+        }
+        
+        switch config.loadTileMode {
+        case .cacheThenServer:
+            diskCache.fetchDataSync(forKey: key,
+                                    failure: {error in tileFromServerFallback()},
+                                    success: {data in result(data, nil) })
+        case .serverThenCache:
+            fetchTileFromServer(failure: {error in tileFromCacheFallback()},
+                                success: {data in result(data, nil) })
+        case .serverOnly:
+            fetchTileFromServer(failure: {error in result(nil, error)},
+                                success: {data in result(data, nil)})
+        case .cacheOnly:
+            diskCache.fetchDataSync(forKey: key,
+                failure: {error in result(nil, error)},
+                success: {data in result(data, nil)})
         }
     }
     
