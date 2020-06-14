020b892e9c588ef6be2afd7d93fdbce02148ddeb
merlos <merlos@users.noreply.github.com>
2020-06-14 13:04:48 -0400

    removed swifter from tests too

--- Example/Tests/MapCacheTests.swift
+++ Example/Tests/MapCacheTests.swift
@@ -11,54 +11,41 @@ import Quick
 import Nimble
 import MapCache
 import MapKit
-import Swifter
 
 class MapCacheTests: QuickSpec {
     override func spec() {
-        // setup a server
-        let server = HttpServer()
-        beforeSuite {
-           let server = HttpServer()
-           server["/ok/:path"] = { .ok(.text("\($0)"))}
-            server["/error/:path"] = { _ in
-                   return .notFound
-            }
-           do {
-             try server.start(1980, forceIPv4: true)
-             print("MapCacheTestServer:: started ( port = \(try server.port()) )")
-           } catch {
-             print("MapCacheTestServer:: server start error: \(error)")
-           }
-          }
-
-        
-        afterSuite {
-            server.stop()
-            print("MapCacheTestServer:: server stopped")
-        }
         describe("MapCache") {
-            let urlTemplate = "a{x}b{y}c{z}d{s}"
+            let urlTemplate = "http://localhost:1980/{s}/{x}/{y}/{z}"
             var config = MapCacheConfig(withUrlTemplate: urlTemplate)
-            config.subdomains = ["a"]
+            config.subdomains = ["ok"]
             let cache = MapCache(withConfig: config)
             let path = MKTileOverlayPath(x: 1, y: 2, z: 3, contentScaleFactor: 1.0)
             
             it("can create the tile url") {
                 let url = cache.url(forTilePath: path)
-                expect(url.absoluteString) == "a1b2c3da"
+                expect(url.absoluteString) == "http://localhost:1980/ok/1/2/3"
             }
             
             it("can generate the key for a path") {
                 let key = cache.cacheKey(forPath: path)
                 expect(key) == "\(urlTemplate)-1-2-3"
             }
-            
-            it("can generate the key for a path") {
-                let key = cache.cacheKey(forPath: path)
-                expect(key) == "\(urlTemplate)-1-2-3"
+            /*
+            it("can fetch tile from server") {
+                cache.fetchTileFromServer(
+                    at: path,
+                    failure: {error in expect(false) == true},
+                    success: {data in expect(true) == true} )
             }
             
-            
+            it("can return error on fetch") {
+                cache.fetchTileFromServer(
+                    at: path,
+                             failure: {error in expect(false) == true},
+                             success: {data in expect(true) == true} )
+                }
+              
+            */
             /*
             it("can do maths") {
                 expect(2) == 2
