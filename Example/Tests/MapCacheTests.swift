48d4556234ffac6f621f9b82380f0c7166640d60
merlos <merlos@users.noreply.github.com>
2020-06-14 11:57:09 -0400

    added test server

--- Example/Tests/MapCacheTests.swift
+++ Example/Tests/MapCacheTests.swift
@@ -11,19 +11,46 @@ import Quick
 import Nimble
 import MapCache
 import MapKit
+import Swifter
 
 class MapCacheTests: QuickSpec {
     override func spec() {
+        // setup a server
+        let server = HttpServer()
+        beforeSuite {
+           let server = HttpServer()
+           server["/ok/:path"] = { .ok(.text("\($0)"))}
+            server["/error/:path"] = { _ in
+                   return .notFound
+            }
+           do {
+             try server.start(1980, forceIPv4: true)
+             print("MapCacheTestServer:: started ( port = \(try server.port()) )")
+           } catch {
+             print("MapCacheTestServer:: server start error: \(error)")
+           }
+          }
+
+        
+        afterSuite {
+            server.stop()
+            print("MapCacheTestServer:: server stopped")
+        }
         describe("MapCache") {
             let urlTemplate = "a{x}b{y}c{z}d{s}"
             var config = MapCacheConfig(withUrlTemplate: urlTemplate)
+            config.subdomains = ["a"]
             let cache = MapCache(withConfig: config)
             let path = MKTileOverlayPath(x: 1, y: 2, z: 3, contentScaleFactor: 1.0)
             
             it("can create the tile url") {
-                config.subdomains = ["a"]
                 let url = cache.url(forTilePath: path)
                 expect(url.absoluteString) == "a1b2c3da"
+            }
+            
+            it("can generate the key for a path") {
+                let key = cache.cacheKey(forPath: path)
+                expect(key) == "\(urlTemplate)-1-2-3"
             }
             
             it("can generate the key for a path") {
