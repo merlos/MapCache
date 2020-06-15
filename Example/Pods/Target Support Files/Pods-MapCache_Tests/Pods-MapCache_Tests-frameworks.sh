2c9922265a9208135e94680e224472c62e89bf0a
merlos <merlos@users.noreply.github.com>
2020-06-15 18:16:27 -0400

    installed OHHTPStubs pos

--- Example/Pods/Target Support Files/Pods-MapCache_Tests/Pods-MapCache_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-MapCache_Tests/Pods-MapCache_Tests-frameworks.sh
@@ -162,10 +162,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/OHHTTPStubs/OHHTTPStubs.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/OHHTTPStubs/OHHTTPStubs.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
