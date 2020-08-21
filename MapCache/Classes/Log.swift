67dabc79d9cb267302a6eaa5cb1bcbb5c0193631
merlos <merlos@users.noreply.github.com>
2020-08-21 18:40:46 -0400

    Added documentation of Log.swift

--- MapCache/Classes/Log.swift
+++ MapCache/Classes/Log.swift
@@ -11,30 +11,58 @@
 
 import Foundation
 
-
+///
+/// A struct to log messages on console.
+///
+/// Format is:
+/// ```
+///   <tag><Level> <message> [with error <error>]
+/// ```
+///
 struct Log {
     
-    fileprivate static let Tag = "[MAPCACHE]"
+    /// The tag
+    fileprivate static let tag = "[MapCache]"
     
+    /// Levels
     fileprivate enum Level : String {
+        /// For displaying messages useful during development.
         case Debug = "[DEBUG]"
+        /// For displaying bad, very, very bad situations.
         case Error = "[ERROR]"
     }
     
+    ///
+    /// The actual method that prints.
+    ///
     fileprivate static func log(_ level: Level, _ message: @autoclosure () -> String, _ error: Error? = nil) {
         if let error = error {
-            print("\(Tag)\(level.rawValue) \(message()) with error \(error)")
+            print("\(tag)\(level.rawValue) \(message()) with error \(error)")
         } else {
-            print("\(Tag)\(level.rawValue) \(message())")
+            print("\(tag)\(level.rawValue) \(message())")
         }
     }
     
+    ///
+    /// For displaying messages. Useful during development of this package.
+    ///
+    /// Example:
+    /// ```
+    /// Log.debug("Hello world") // prints: [MapCache][DEBUG] Hello word
+    /// ```
+    ///
+    /// These messages are displayed only if DEBUG is defined
+    ///
     static func debug(message: @autoclosure () -> String, error: Error? = nil) {
         #if DEBUG
         log(.Debug, message(), error)
         #endif
     }
     
+    ///
+    /// These messages are displayed independently of the debug mode.
+    /// Used  to provide useful information on exceptional situations to library users.
+    ///
     static func error(message: @autoclosure () -> String, error: Error? = nil) {
         log(.Error, message(), error)
     }
