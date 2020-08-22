39b953474cc08368a07f08ec0b4fd2b89658c835
merlos <merlos@users.noreply.github.com>
2020-08-22 10:06:28 -0400

    Completed MapCacheConfig documentation

--- MapCache/Classes/MapCacheConfig.swift
+++ MapCache/Classes/MapCacheConfig.swift
@@ -15,9 +15,21 @@ import CoreGraphics
 ///
 public struct MapCacheConfig  {
    
+    /// Each time a tile is going to be retrieved from the server its x,y and z (zoom) values are plugged into this URL template.
     ///
+    /// Default value `"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`
+    ///
+    /// Where
+    ///   1. `{s}` Replaced with one of the subdomains defined in `subdomains`.
+    ///   2. `{z}` Replaced with the zoom level value.
+    ///   3. `{x}` Replaced with the position of the tile in the X asis for this zoom level
+    ///   4. `{y}` Replaced with the position of the tile in the X asis for this zoom level
+    ///
+    /// - SeeAlso: [Tiles servers in OpenStreetMap wiki](https://en.wikipedia.org/wiki/Tiled_web_map)
+    /// - SeeAlso: [Tiled we maps in wikipedia](https://en.wikipedia.org/wiki/Tiled_web_map)
     public var urlTemplate: String = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     
+    /// Subdomains used on the `urlTemplate`
     public var subdomains: [String] = ["a","b","c"]
     
     ///
@@ -51,23 +63,39 @@ public struct MapCacheConfig  {
     public var capacity: UInt64 = UINT64_MAX
     
     ///
-    /// Tile size
+    /// Tile size of the tile. Default is 256x256
     ///
     public var tileSize: CGSize = CGSize(width: 256, height: 256)
 
     ///
     /// Load tile  mode.
-    /// Sets the strategy when loading a tile. By default loads from the cache and if it fails loads from the server
+    /// Sets the strategy to be used when loading a tile.
+    /// By default loads from the cache and if it fails loads from the server
     ///
+    /// - SeeAlso: LoadTileMode
+    
     public var loadTileMode: LoadTileMode = .cacheThenServer
     
+    ///
+    /// Constructor with all the default values.
+    ///
     public init() {
     }
     
+    ///
+    ///Constructor that overwrites the urlTemplate
+    ///
+    /// - Parameter withUrlTemplate is the string of the urlTemplate
+    ///
+    /// - SeeAlso: MapCacheConfig.urlTemplate
+    ///
     public init(withUrlTemplate urlTemplate: String)  {
         self.urlTemplate = urlTemplate
     }
     
+    ///
+    /// Selects one of the subdomains randomly.
+    ///
     public func randomSubdomain() -> String? {
         if subdomains.count == 0 {
             return nil
@@ -76,13 +104,13 @@ public struct MapCacheConfig  {
         return subdomains[rand]
     }
     
-    /// Keeps track of the last subdomain requested.
+    /// Keeps track of the index of the last subdomain requested for round robin
     private var subdomainRoundRobin: Int = 0
     
     /// Round Robin algorithm
     /// If subdomains are a,b,c then it makes requests to a,b,c,a,b,c,a,b,c...
     ///
-    /// It uniformly makes requests
+    /// It uniformly makes requests to all the subdomains.
     public mutating func roundRobinSubdomain() -> String? {
         if subdomains.count == 0 {
             return nil
