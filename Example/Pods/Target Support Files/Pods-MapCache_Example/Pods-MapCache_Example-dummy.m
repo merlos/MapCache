dcf461513e4e98954bf322d0f38c2a7e098819c0
Carlos García <carlosypunto@gmail.com>
2020-11-09 01:19:02 +0100

    update example podfile and remove pods directory from repo

--- Example/Pods/Target Support Files/Pods-MapCache_Example/Pods-MapCache_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MapCache_Example : NSObject
-@end
-@implementation PodsDummy_Pods_MapCache_Example
-@end
--- Example/Pods/Target Support Files/Pods-MapCache_Tests/Pods-MapCache_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MapCache_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_MapCache_Tests
-@end
