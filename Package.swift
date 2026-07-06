ccada4fd7102a050d04159e4f8745e2b89767aa1
merlos <merlos@users.noreply.github.com>
2026-07-06 11:27:59 +0300

    bump macos to 11.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MapCache",
     platforms: [
-        .iOS(.v14), .macOS(.v10_13)
+        .iOS(.v14), .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
